bd754307782186bbb9c6a8fe0645e8dcf56beb40
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-11-03 00:06:26 +0100

    add link to bgg post

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,13 +11,11 @@
   <p v-html="t('home.play1')"></p>
   <p v-html="t('home.play2')"></p>
 
-<!--
   <p class="text-muted fst-italic">
     {{t('home.feedback')}}
-    <a href="https://boardgamegeek.com/thread/XXX/brdgmme-skymines-solo-helper-web-application" target="_blank" rel="noopener">Board Game Geek Thread</a> |
+    <a href="https://boardgamegeek.com/thread/2964243/brdgmme-skymines-solo-helper-web-application" target="_blank" rel="noopener">Board Game Geek Thread</a> |
     <a href="https://github.com/brdgm/skymines-solo-helper" target="_blank" rel="noopener">GitHub</a>
   </p>
--->
 
   <router-link to="/setupGame" class="btn btn-primary btn-lg mt-3">{{t('action.playGame')}}</router-link>
 
