10b1ff46645256d039f8a595ff4e8af7ec314681
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-02-17 20:26:40 +0100

    add matomo tracking

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
 import { RouteRecordRaw } from 'vue-router'
+import createRouterMatomoTracking from 'brdgm-commons/src/util/router/createRouterMatomoTracking'
+import { name, version, appDeployName } from '@/../package.json'
 import AppHome from '@/views/AppHome.vue'
 import NotFound from '@/views/NotFound.vue'
 import SetupGame from '@/views/SetupGame.vue'
@@ -9,8 +11,6 @@ import TurnBot from '@/views/TurnBot.vue'
 import EndOfRound from '@/views/EndOfRound.vue'
 import EndOfGame from '@/views/EndOfGame.vue'
 import StandaloneScoring from '@/views/StandaloneScoring.vue'
-import createRouter from 'brdgm-commons/src/util/router/createRouter'
-import { name } from '@/../package.json'
 
 const LOCALSTORAGE_KEY = `${name}.route`
 
@@ -67,4 +67,4 @@ const routes: Array<RouteRecordRaw> = [
   }
 ]
 
-export default createRouter(routes, LOCALSTORAGE_KEY, 'AppHome')
+export default createRouterMatomoTracking(routes, LOCALSTORAGE_KEY, appDeployName, version, 'AppHome')
